c109609913c613477c97223cf23c67d358c01d4c
Miras <iskakov91633@gmail.com>
2023-12-29 14:10:40 +0600

    Added button

--- WorkoutApp/Controller/Session/SessionController.swift
+++ WorkoutApp/Controller/Session/SessionController.swift
@@ -10,7 +10,7 @@ import UIKit
 class SessionController: BaseController {
 
     private let timerView: BaseInfoView = {
-        let view = BaseInfoView(with: "test", aligment: .center)
+        let view = BaseInfoView(with: "test", buttonTitle: "button")
         return view
     }()
     override func viewDidLoad() {
